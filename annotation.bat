2366b0b235b0b0bf556c617742c1f463ee144893
rvteja24 <39586886+rvteja24@users.noreply.github.com>
2019-04-03 18:23:30 +0530

    Create annotation.bat

---
+++ annotation.bat
@@ -0,0 +1,3 @@
+cd C:\Users\A610547\Downloads\opencv\build\x86\vc12\bin
+
+opencv_annotation -images C:/Users/A610547/Desktop/trafficProcessing/trainingCode/positiveSamples -annotations C:/Users/A610547/Desktop/trafficProcessing/trainingCode/trainedClassifier
